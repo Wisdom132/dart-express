541e0a0ffd3a283de1aea33b64980b4464e0c91d
wisdom <wisdomekpot@gmail.com>
2019-02-26 16:05:29 +0100

    express

---
+++ progress.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#icon').on('click',function() {
+        $('#faq').toggle();
+    })
+    $('#icon2').on('click', function () {
+        $('#faq2').toggle();
+    })
+    $('#icon3').on('click', function () {
+        $('#faq3').toggle();
+    })
+    $('#icon4').on('click', function () {
+        $('#faq4').toggle();
+    })
+    $('#icon5').on('click', function () {
+        $('#faq5').toggle();
+    })
+    $('#icon6').on('click', function () {
+        $('#faq6').toggle();
+    })
+    $('#icon7').on('click', function () {
+        $('#faq7').toggle();
+    })
+    $('#icon8').on('click', function () {
+        $('#faq8').toggle();
+    })
+    $('#icon9').on('click', function () {
+        $('#faq9').toggle();
+    })
+    $('#faq').hide();
+    $('#faq2').hide();
+    $('#faq3').hide();
+    $('#faq4').hide();
+    $('#faq5').hide();
+    $('#faq6').hide();
+    $('#faq7').hide();
+    $('#faq8').hide();
+    $('#faq9').hide();
+
+})
